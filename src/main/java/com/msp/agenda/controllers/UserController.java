24713f78c151107a729b4466d450e2756c6615f2
Mateus Silva Pereira <mateussilvapereira2018@gmail.com>
2023-04-27 10:55:56 -0400

    Create User controller end refactory user

---
+++ src/main/java/com/msp/agenda/controllers/UserController.java
@@ -0,0 +1,130 @@
+package com.msp.agenda.controllers;
+
+import java.net.URI;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.msp.agenda.models.User;
+import com.msp.agenda.models.User.CreateUser;
+import com.msp.agenda.models.User.UpdateUser;
+import com.msp.agenda.services.UserServices;
+
+@RestController
+@RequestMapping("/user")
+@Validated
+public class UserController {
+
+	
+	@Autowired
+	private UserServices userServices;
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		return ResponseEntity.ok()
+				.body(userServices.findById(id));
+	}
+	
+	@PostMapping()
+	@Validated(CreateUser.class)
+	public ResponseEntity<Void> create(@Valid @RequestBody User obj){
+		userServices.create(obj);
+		URI uri =ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/id").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@PutMapping("/{id}")
+	@Validated(UpdateUser.class)
+	public ResponseEntity<User> update(@Valid @RequestBody User obj, @PathVariable Long id){
+		userServices.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		userServices.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/msp/agenda/models/User.java
+++ src/main/java/com/msp/agenda/models/User.java
@@ -14,6 +14,7 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
@@ -78,8 +79,17 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+    
+    @JsonIgnore
+    public List<Task> getTasks() {
+		return tasks;
+	}
 
-    @Override
+	public void setTasks(List<Task> tasks) {
+		this.tasks = tasks;
+	}
+
+	@Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
