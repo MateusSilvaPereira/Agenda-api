3214dbec55367536d30d3e61dff970c5f550b66e
Mateus Silva Pereira <mateussilvapereira2018@gmail.com>
2023-05-12 12:58:36 -0400

    Add password and encoder and create ProfileEnum

--- src/main/java/com/msp/agenda/services/UserServices.java
+++ src/main/java/com/msp/agenda/services/UserServices.java
@@ -1,12 +1,16 @@
 package com.msp.agenda.services;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.msp.agenda.models.User;
+import com.msp.agenda.models.enums.ProfileEnum;
 import com.msp.agenda.respository.UserRepository;
 import com.msp.agenda.services.exceptions.DataBindingViolationException;
 import com.msp.agenda.services.exceptions.ObjectNotFoundException;
@@ -14,6 +18,10 @@ import com.msp.agenda.services.exceptions.ObjectNotFoundException;
 @Service
 public class UserServices {
 
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
 	@Autowired
 	private UserRepository userRepository;
 	
@@ -26,6 +34,8 @@ public class UserServices {
 	@Transactional
 	public User create(User obj) {
 		obj.setId(null);
+		obj.setPassword(bCryptPasswordEncoder.encode(obj.getPassword()));
+		obj.setProfiles(Stream.of(ProfileEnum.USER.getCode()).collect(Collectors.toSet()));
 		obj = userRepository.save(obj);
 		return obj;
 	}
@@ -34,6 +44,7 @@ public class UserServices {
 	public User update(User obj) {
 		User newObj = findById(obj.getId());
 		newObj.setPassword(obj.getPassword());
+		newObj.setPassword(bCryptPasswordEncoder.encode(newObj.getPassword()));;
 		return userRepository.save(newObj);
 	}
 	
