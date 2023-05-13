8aea12d1353a0da1d6d9e888e8099b1e32ddb638
Mateus Silva Pereira <mateussilvapereira2018@gmail.com>
2023-05-13 09:03:22 -0400

    Create UserDetailsServiceImpl and JWTUtil

--- src/main/java/com/msp/agenda/configs/SecurityConfig.java
+++ src/main/java/com/msp/agenda/configs/SecurityConfig.java
@@ -2,24 +2,39 @@ package com.msp.agenda.configs;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import com.msp.agenda.security.JWTUtil;
+
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
+	
+	@SuppressWarnings("unused")
+	private AuthenticationManager authenticationManager;
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Autowired
+	private JWTUtil jwtUtil;
 	
 	private static final String [] PUBLIC_MATHERS = {
 			"/"
@@ -34,6 +49,14 @@ public class SecurityConfig {
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		
 		http.cors().and().csrf().disable();
+		
+		AuthenticationManagerBuilder authenticationManagerBuilder = http
+				.getSharedObject(AuthenticationManagerBuilder.class);
+		
+		authenticationManagerBuilder.userDetailsService(this.userDetailsService)
+				.passwordEncoder(bCryptPasswordEcoder());
+		
+		this.authenticationManager = authenticationManagerBuilder.build();
 		
 		http.authorizeRequests()
 				.antMatchers(HttpMethod.POST, PUBLIC_MATHERS_POST).permitAll()
--- src/main/java/com/msp/agenda/respository/UserRepository.java
+++ src/main/java/com/msp/agenda/respository/UserRepository.java
@@ -1,11 +1,17 @@
 package com.msp.agenda.respository;
 
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.msp.agenda.models.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
+
+	@Transactional(readOnly = true)
+	User findByUserName(String username);
 
 }
